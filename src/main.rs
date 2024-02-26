ec06d4a51887f18931633a8513112cb0190908e0
Mrityunjay Kumar <mk10399@gmail.com>
2024-02-26 23:02:27 +0530

    loop, while, for

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,68 @@
-fn main() {
-    let number = 3;
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+/* 
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+*/
+
+
+/*
+fn main() {
+    let mut count = 0;
+    // loop with label
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            // breaks inner loop
+            if remaining == 9 {
+                break;
+            }
+            // breaks outer loop
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+*/
+
+fn main() {
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}!");
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+        index += 1;
     }
 
-    // will give error - doesn't works like javascript
-    // if number {
-    //     println!("number was three");
-    // }
+    for element in a {
+        println!("the value is: {element}");
+    }
 
-    // Using if in a let Statement
-    let condition = true;
-    let number = if condition { 5 } else { 6 };
-    println!("The value of number is: {number}");
-    
-    // error (both the if arm and the else arm types are mismatched)
-    // let number = if condition { 5 } else { "six" }; 
-
-
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
 }
